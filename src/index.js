47979747302fa354e8a7e054a107f5bf0e4a4a54
Tom Keech <45048917+mot667@users.noreply.github.com>
2020-12-19 17:36:24 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
+ReactDOM.render(   
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>, document.getElementById('root'));
