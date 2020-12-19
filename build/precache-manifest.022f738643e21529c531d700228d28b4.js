bb87f33e4951624d1a045bf95fbb17d929a98e4e
Tom Keech <45048917+mot667@users.noreply.github.com>
2020-12-19 17:38:23 -0500

    Build
    npm run build

---
+++ build/precache-manifest.022f738643e21529c531d700228d28b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b8b417d9b213a4c19f4ec8bba88c4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abdef99d29f590546b04",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd605066bea6a1bce8cc",
+    "url": "/static/css/main.58531e33.chunk.css"
+  },
+  {
+    "revision": "abdef99d29f590546b04",
+    "url": "/static/js/2.2d87885d.chunk.js"
+  },
+  {
+    "revision": "6e955cdac22de8c1616c1bb714390207",
+    "url": "/static/js/2.2d87885d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd605066bea6a1bce8cc",
+    "url": "/static/js/main.cd946812.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8ac3b5d4477a35d7c4e9f75fc077358d",
+    "url": "/static/media/gmailLogo.8ac3b5d4.png"
+  },
+  {
+    "revision": "c9e789161cfcee0ca496af22be0a3620",
+    "url": "/static/media/instagram.c9e78916.png"
+  },
+  {
+    "revision": "f756c437aa723da0b60656324b850044",
+    "url": "/static/media/me.f756c437.jpg"
+  },
+  {
+    "revision": "ff8d3ff0b78d716dc7f20b15a748d55f",
+    "url": "/static/media/meWallPaper.ff8d3ff0.jpg"
+  },
+  {
+    "revision": "bb1ea5b7a2aee01f985bf7f8743d06bb",
+    "url": "/static/media/tomKeechResume.bb1ea5b7.pdf"
+  }
+]);
