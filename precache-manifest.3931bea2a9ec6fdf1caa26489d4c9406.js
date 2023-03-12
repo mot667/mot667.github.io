7bb0bacdb6f1f70fb00e0300ba52f7cfbd6c0382
Tom Keech <45048917+mot667@users.noreply.github.com>
2023-03-12 15:52:25 -0400

    Revert "New build"
    This reverts commit a9321df65369b98ebd1ef7d7f6934e6a395362c5.

---
+++ precache-manifest.3931bea2a9ec6fdf1caa26489d4c9406.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809c9ce3debc81ef05806fc1f05912f0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "383044afa2c143c2bc7d",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c1bf47d2322f3ea2733e",
+    "url": "./static/css/main.c7c7db8a.chunk.css"
+  },
+  {
+    "revision": "383044afa2c143c2bc7d",
+    "url": "./static/js/2.a153466f.chunk.js"
+  },
+  {
+    "revision": "6e955cdac22de8c1616c1bb714390207",
+    "url": "./static/js/2.a153466f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bf47d2322f3ea2733e",
+    "url": "./static/js/main.3937d1d0.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  },
+  {
+    "revision": "8ac3b5d4477a35d7c4e9f75fc077358d",
+    "url": "./static/media/gmailLogo.8ac3b5d4.png"
+  },
+  {
+    "revision": "c9e789161cfcee0ca496af22be0a3620",
+    "url": "./static/media/instagram.c9e78916.png"
+  },
+  {
+    "revision": "f756c437aa723da0b60656324b850044",
+    "url": "./static/media/me.f756c437.jpg"
+  },
+  {
+    "revision": "ff8d3ff0b78d716dc7f20b15a748d55f",
+    "url": "./static/media/meWallPaper.ff8d3ff0.jpg"
+  },
+  {
+    "revision": "277a76bc462ae68f70e03ecb7c6a0b58",
+    "url": "./static/media/tomKeechResume.277a76bc.pdf"
+  }
+]);
